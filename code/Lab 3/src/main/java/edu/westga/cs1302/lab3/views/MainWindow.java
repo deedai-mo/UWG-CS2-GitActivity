bb0687bf1a1b2d6fd14394a28587129453733bc2
deedai-mo <ellamogaha@gmail.com>
2025-09-04 10:15:01 -0400

    Lab 3 stater file made
    I have currently added the lab 3 javafx file to my repo

---
+++ code/Lab 3/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -0,0 +1,29 @@
+package edu.westga.cs1302.lab3.views;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+/**
+ * Controller class for drawing various things to our canvas window.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class MainWindow {
+	    @FXML
+	    private TextField Input;
+
+	    @FXML
+	    private TextArea output;
+
+	    @FXML
+	    void displaytext(ActionEvent event) {
+
+	    } 
+    /**
+     * Perform any needed initialization of UI components and underlying objects.
+     */
+    public void initialize() {
+    	
+    }
+}
