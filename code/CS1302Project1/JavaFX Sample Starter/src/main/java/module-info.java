30ae3545787ed43025f55e2cc1549529a5bb80b8
deedai-mo <ellamogaha@gmail.com>
2025-09-24 02:51:37 -0400

    Error found

--- code/CS1302Project1/JavaFX Sample Starter/src/main/java/module-info.java
+++ code/CS1302Project1/JavaFX Sample Starter/src/main/java/module-info.java
@@ -2,8 +2,7 @@ module edu.westga.cs1302.javafx_sample_starter {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-	requires javafx.base;
 
-    opens edu.westga.cs1302.tasktracker.views to javafx.fxml;
-    exports edu.westga.cs1302.tasktracker;
+    opens edu.westga.cs1302.javafx_sample_starter.views to javafx.fxml;
+    exports edu.westga.cs1302.javafx_sample_starter;
 }
--- code/JavaFX Sample Starter - AnchorPane/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/tasktracker/model/Priority.java
+++
@@ -1,6 +0,0 @@
-package edu.westga.cs1302.tasktracker.model;
-
-public enum Priority {
-	LOW, MEDIUM, HIGH;
-
-}
