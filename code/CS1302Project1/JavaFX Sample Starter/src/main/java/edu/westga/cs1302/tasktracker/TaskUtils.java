55b7a731df0580ff81a54522443a01b1d53dc8f0
deedai-mo <ellamogaha@gmail.com>
2025-09-25 12:11:47 -0400

    Project 1 restart
    I am restarting project 1 caue the Junit file has been given

--- code/CS1302Project1/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/tasktracker/TaskUtils.java
+++ code/CS1302Project1/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/tasktracker/TaskUtils.java
@@ -2,11 +2,18 @@ package edu.westga.cs1302.tasktracker;
 
 import java.util.List;
 
+/**
+ * A utility class for performing operations on Task objects.
+ *
+ * @author CS 1302
+ * @version Fall 2025
+ */
 public class TaskUtils {
-	/**
+    /**
      * Counts the number of tasks in a list that have a specific priority.
-     * * @param priority The priority to count.
-     * @param tasks The list of tasks to search through.
+     *
+     * @param priority The priority to count.
+     * @param tasks    The list of tasks to search through.
      * @return The number of tasks with the specified priority.
      */
     public static int countTasksByPriority(String priority, List<Task> tasks) {
@@ -18,5 +25,4 @@ public class TaskUtils {
         }
         return count;
     }
-
 }
