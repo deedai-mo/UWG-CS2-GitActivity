b6252badebe03d4eda50b28c579fa52862ce6729
deedai-mo <ellamogaha@gmail.com>
2025-10-15 14:50:01 -0400

    Ascending Comparator
    I have created a comparator class that compares two tasks in ascending order

---
+++ code/TaskTracker/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/Ascending.java
@@ -0,0 +1,30 @@
+package edu.westga.cs1302.task_tracker.model;
+
+import java.util.Comparator;
+
+public class Ascending implements Comparator<Task> {
+	private static final String COMPARATOR_NAME = "Ascending Priority (LOW -> HIGH)";
+	
+	/**
+     * Compares its two Task arguments for order.
+     * * @param task1 the first Task to be compared
+     * @param task2 the second Task to be compared
+     * @return a negative integer, zero, or a positive integer as task1's priority
+     * is less than, equal to, or greater than task2's priority.
+     */
+	@Override 
+	public int compare(Task task1, Task task2) {
+		return task2.getPriority().compareTo(task1.getPriority());
+		
+	}
+	/**
+     * Returns the name of the comparator to display in the UI.
+     * * @return the name of the comparator
+     */
+	@Override
+	public String toString() {
+		return COMPARATOR_NAME;
+	}
+	
+
+}
